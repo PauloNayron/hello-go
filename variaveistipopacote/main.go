c873e7affa5f38da5d31e7dfedb12eb8912a7b98
PauloNayron <paulonayron.s@hotmail.com>
2021-07-13 18:21:33 -0300

    feat: implementação de variáveis, tipos e pacote

---
+++ variaveistipopacote/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"./math"
+	"fmt"
+)
+
+func main() {
+	// a := "Hello World!"
+	// a = "Olá, mundo!"
+
+	// fmt.Printf("%T \n", a)
+
+	resultado := math.Soma(1, 2)
+	fmt.Printf("%T \n", resultado)
+}
